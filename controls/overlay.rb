6c897929d93144ab69102b30a8341aa76f899c52
karikarshivani <karikarshivani@gmail.com>
2022-04-13 21:47:16 -0400

    Updated Overlay.rb to skip inapplicable controls and drafted README
    Signed-off-by: karikarshivani <karikarshivani@gmail.com>

---
+++ controls/overlay.rb
@@ -0,0 +1,52 @@
+# encoding: utf-8
+
+include_controls "microsoft-sql-server-2017-cis-baseline" do
+  control "microsoft-sql-server-2017-cis-2.9" do
+    impact 0.0
+    describe 'According to this CIS benchmark, this requirement is not applicable to AWS RDS instances of this type of database.' do
+      skip 'According to this CIS benchmark, this requirement is not applicable to AWS RDS instances of this type of database.'
+    end
+  end
+
+  control "microsoft-sql-server-2017-cis-2.10" do
+    impact 0.0
+    describe 'According to this CIS benchmark, this requirement is not applicable to AWS RDS instances of this type of database.' do
+      skip 'According to this CIS benchmark, this requirement is not applicable to AWS RDS instances of this type of database.'
+    end
+  end
+
+  control "microsoft-sql-server-2017-cis-2.12" do
+    impact 0.0
+    describe 'According to this CIS benchmark, this requirement is not applicable to AWS RDS instances of this type of database.' do
+      skip 'According to this CIS benchmark, this requirement is not applicable to AWS RDS instances of this type of database.'
+    end
+  end
+
+  control "microsoft-sql-server-2017-cis-3.11" do
+    impact 0.0
+    describe 'According to this CIS benchmark, this requirement is not applicable to AWS RDS instances of this type of database.' do
+      skip 'According to this CIS benchmark, this requirement is not applicable to AWS RDS instances of this type of database.'
+    end
+  end
+
+  control "microsoft-sql-server-2017-cis-4.1" do
+    impact 0.0
+    describe 'According to this CIS benchmark, this requirement is not applicable to AWS RDS instances of this type of database.' do
+      skip 'According to this CIS benchmark, this requirement is not applicable to AWS RDS instances of this type of database.'
+    end
+  end
+
+  control "microsoft-sql-server-2017-cis-6.1" do
+    impact 0.0
+    describe 'According to this CIS benchmark, this requirement is not applicable to AWS RDS instances of this type of database.' do
+      skip 'According to this CIS benchmark, this requirement is not applicable to AWS RDS instances of this type of database.'
+    end
+  end
+
+  control "microsoft-sql-server-2017-cis-8.1" do
+    impact 0.0
+    describe 'According to this CIS benchmark, this requirement is not applicable to AWS RDS instances of this type of database.' do
+      skip 'According to this CIS benchmark, this requirement is not applicable to AWS RDS instances of this type of database.'
+    end
+  end
+end
